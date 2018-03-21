a49c45b57dc3a7cc41d8441c8160d381538c598e
Yusei Nishiyama <allegllet.scherzand@gmail.com>
2018-03-21 06:45:03 +0000

    Routines

--- App.js
+++ App.js
@@ -6,53 +6,51 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
+  FlatList,
   StyleSheet,
   Text,
-  View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const rows = [
+  {id: 0, text: 'Day 1'},
+  {id: 1, text: 'Day 2'},
+  {id: 2, text: 'Day 3'},
+  {id: 3, text: 'Day 4'},
+  {id: 4, text: 'Day 5'}
+]
 
-type Props = {};
-export default class App extends Component<Props> {
+const extractKey = ({id}) => id
+
+export default class App extends Component {
+
+  renderItem = ({item}) => {
+    return (
+      <Text style={styles.row}>
+        {item.text}
+      </Text>
+    )
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <FlatList
+        style={styles.container}
+        data={rows}
+        renderItem={this.renderItem}
+        keyExtractor={extractKey}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 20,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  row: {
+    padding: 20,
+    marginBottom: 1,
+    backgroundColor: 'skyblue'
   },
 });
