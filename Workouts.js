06d99122837175c95b5a74b4afa28b9cf101b54c
Yusei Nishiyama <allegllet.scherzand@gmail.com>
2018-03-22 00:58:53 +0000

    Handle tap action

--- Workouts.js
+++ Workouts.js
@@ -1,37 +1,60 @@
 import React, { Component } from 'react';
 import {
+  View,
   FlatList,
   StyleSheet,
   Text,
+  TouchableOpacity
 } from 'react-native';
 
 const rows = [
-  {id: 0, text: 'Day 1'},
-  {id: 1, text: 'Day 2'},
-  {id: 2, text: 'Day 3'},
-  {id: 3, text: 'Day 4'},
-  {id: 4, text: 'Day 5'}
+  {id: '0', title: 'Day 1'},
+  {id: '1', title: 'Day 2'},
+  {id: '2', title: 'Day 3'},
+  {id: '3', title: 'Day 4'},
+  {id: '4', title: 'Day 5'},
 ]
 
-const extractKey = ({id}) => id
+class WorkoutItem extends React.PureComponent {
+
+  _onPress = () => {
+    this.props.onPressItem(this.props.id);
+  };
+
+  render() {
+    return (
+      <TouchableOpacity onPress={this._onPress}>
+        <View style={styles.row}>
+          <Text>{this.props.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
 
 export default class Workouts extends Component {
 
-  renderItem = ({item}) => {
-    return (
-      <Text style={styles.row}>
-        {item.text}
-      </Text>
-    )
-  }
+  _renderItem = ({item}) => (
+    <WorkoutItem
+      id={item.id}
+      onPressItem={this._onPressItem}
+      title={item.title}
+    />
+  );
+
+  _keyExtractor = (item, index) => item.id;
+
+  _onPressItem = (id) => {
+    console.log(id)
+  };
 
   render() {
     return (
       <FlatList
         style={styles.container}
         data={rows}
-        renderItem={this.renderItem}
-        keyExtractor={extractKey}
+        renderItem={this._renderItem}
+        keyExtractor={this._keyExtractor}
       />
     );
   }
