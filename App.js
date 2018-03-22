0dff311e0bd66701d7a14a7caa059d8ddf44941c
Yusei Nishiyama <allegllet.scherzand@gmail.com>
2018-03-22 00:20:08 +0000

    Add route navigator

--- App.js
+++ App.js
@@ -1,43 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  FlatList,
   StyleSheet,
-  Text,
+  NavigatorIOS
 } from 'react-native';
-
-const rows = [
-  {id: 0, text: 'Day 1'},
-  {id: 1, text: 'Day 2'},
-  {id: 2, text: 'Day 3'},
-  {id: 3, text: 'Day 4'},
-  {id: 4, text: 'Day 5'}
-]
-
-const extractKey = ({id}) => id
+import Workouts from './Workouts'
 
 export default class App extends Component {
 
-  renderItem = ({item}) => {
-    return (
-      <Text style={styles.row}>
-        {item.text}
-      </Text>
-    )
-  }
-
   render() {
     return (
-      <FlatList
+      <NavigatorIOS
         style={styles.container}
-        data={rows}
-        renderItem={this.renderItem}
-        keyExtractor={extractKey}
+        initialRoute={{
+          title: 'Workouts',
+          component: Workouts,
+        }}
       />
     );
   }
@@ -45,12 +22,6 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 20,
     flex: 1,
-  },
-  row: {
-    padding: 20,
-    marginBottom: 1,
-    backgroundColor: 'skyblue'
   },
 });
---
+++ Workouts.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+const rows = [
+  {id: 0, text: 'Day 1'},
+  {id: 1, text: 'Day 2'},
+  {id: 2, text: 'Day 3'},
+  {id: 3, text: 'Day 4'},
+  {id: 4, text: 'Day 5'}
+]
+
+const extractKey = ({id}) => id
+
+export default class Workouts extends Component {
+
+  renderItem = ({item}) => {
+    return (
+      <Text style={styles.row}>
+        {item.text}
+      </Text>
+    )
+  }
+
+  render() {
+    return (
+      <FlatList
+        style={styles.container}
+        data={rows}
+        renderItem={this.renderItem}
+        keyExtractor={extractKey}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    flex: 1,
+  },
+  row: {
+    padding: 20,
+    marginBottom: 1,
+    backgroundColor: 'skyblue'
+  },
+});
